8c3df48b854f7a27fa30b167314a568ff7b7df43
Ibrahim Khdairat <82310547+Ibrahim-Khdairat@users.noreply.github.com>
2024-12-16 16:27:53 +0300

    new update

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
-const DIGITALCLOCK = document.querySelector('.digitalClock h1');
+const DIGITALCLOCK = document.querySelector('.digitalClock #time');
+const DIGITALDATE  = document.querySelector('.digitalClock #date');
 
 function leadingZero(time) {
     if (time <= 9) {
@@ -16,6 +17,16 @@ function runClock() {
     let min = date.getMinutes();
     let sec = date.getSeconds();
 
+    let dayNum = date.getDate();
+    let day = date.getDay();
+    let month = date.getMonth();
+    let year = date.getFullYear();
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const months = [
+        'January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'
+      ];
+
     let hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12);
     let minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60);
     let secPosition = sec * 360 / 60;
@@ -23,7 +34,8 @@ function runClock() {
     HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
     MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
     SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
-    DIGITALCLOCK.textContent = leadingZero(hr) + ":" + leadingZero(min) + ":" + leadingZero(sec);
+    DIGITALCLOCK.textContent =leadingZero(hr) + ":" + leadingZero(min) + ":" + leadingZero(sec);
+    DIGITALDATE.textContent = daysOfWeek[day]+ ", " + dayNum+ "-" + months[month]+ "-" + year;
 }
 
 var interval = setInterval(runClock, 1000);
